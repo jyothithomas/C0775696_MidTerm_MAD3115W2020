c83278d6acfe32d1d50d7e5ebaf5b8810183784b
Jyothi Thomas <jyothitomas.05@gmail.com>
2020-03-07 19:22:52 -0500

    [JT] few changes done in main storyboard

--- C0775696_MidTerm_MAD3115W2020/ShowBillDetailsViewController.swift
+++ C0775696_MidTerm_MAD3115W2020/ShowBillDetailsViewController.swift
@@ -9,12 +9,8 @@
 import UIKit
 
 class ShowBillDetailsViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        // Do any additional setup after loading the view.
     }
     
 
