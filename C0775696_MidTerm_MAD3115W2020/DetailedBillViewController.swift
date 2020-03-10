681d8fde78a96a7047f3d844510914055acb81b6
jyothi <jyothithomas.05@gmail.com>
2020-03-10 18:31:56 -0400

    [JT] working on adding bills

---
+++ C0775696_MidTerm_MAD3115W2020/DetailedBillViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailedBillViewController.swift
+//  C0775696_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class DetailedBillViewController: UIViewController {
+
+    @IBOutlet weak var tblDetailedBillview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.hidesBarsOnTap = true
+        self.navigationController?.title = "Bill Details"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
