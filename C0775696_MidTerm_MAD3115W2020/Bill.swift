9ed6edc0547b18006c71d4c0e7483dd70af681ce
Jyothi Thomas <jyothithomas.05@gmail.com>
2020-03-10 16:16:27 -0400

    [JT] Bill class Added

---
+++ C0775696_MidTerm_MAD3115W2020/Bill.swift
@@ -0,0 +1,31 @@
+//
+//  Bill.swift
+//  C0775696_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+enum BillType
+{
+    case INTERNET, MOBILE, HYDRO, INSURANCE
+}
+
+class Bill
+{
+    
+    var billID: String
+    var billDate: Date
+    var billType: BillType
+    var billAmount: Double = 0.0
+    
+    init(billID: String, billDate: Date, billType: BillType)
+    {
+        self.billID = billID
+        self.billDate = billDate
+        self.billType = billType
+    }
+}
+
