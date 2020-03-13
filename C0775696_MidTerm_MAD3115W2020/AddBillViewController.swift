75dd0c0c45bdabe78e00ef1225231e56869744c5
jyoti <jyothithomas.05@gmail.com>
2020-03-13 18:48:35 -0400

    [JT] commit date picker view added

--- C0775696_MidTerm_MAD3115W2020/AddBillViewController.swift
+++ C0775696_MidTerm_MAD3115W2020/AddBillViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class AddBillViewController: UIViewController {
+class AddBillViewController: UIViewController, UITextFieldDelegate {
     var customer: Customer?
     
     @IBOutlet weak var txtBillID: UITextField!
     @IBOutlet weak var txtBillDate: UITextField!
+    var datePicker: UIDatePicker!
     @IBOutlet weak var txtInternetProviderName: UITextField!
     @IBOutlet weak var txtInternetGBUsed: UITextField!
     @IBOutlet weak var txtMobileManufacturer: UITextField!
@@ -25,22 +26,110 @@ class AddBillViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Add New Bill"
-        //self.navigationItem.hidesBackButton = true
+        txtBillDate.delegate = self
         
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickUpDate(self.txtBillDate)
+    }
+    
+    
+    func pickUpDate(_ textField: UITextField)
+    {
+      //Date Picker
+      self.datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+      self.datePicker.backgroundColor = UIColor.white
+      self.datePicker.datePickerMode = UIDatePicker.Mode.date
+      textField.inputView = self.datePicker
+       
+      //Toolbar
+      let toolBar = UIToolbar()
+      toolBar.barStyle = .default
+      toolBar.isTranslucent = true
+      toolBar.tintColor = .red
+      toolBar.sizeToFit()
+       
+      //Adding Button Toolbar
+      let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddBillViewController.doneClick))
+      let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+      let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddBillViewController.cancelClick))
+      toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+      toolBar.isUserInteractionEnabled = true
+      textField.inputAccessoryView = toolBar
+    }
+     
+    //Button Done and Cancel
+    @objc func doneClick()
+    {
+      let dateformatter1 = DateFormatter()
+      dateformatter1.dateStyle = .medium
+      dateformatter1.timeStyle = .none
+      txtBillDate.text = dateformatter1.string(from: datePicker.date)
+      txtBillDate.resignFirstResponder()
+    }
+     
+    @objc func cancelClick()
+    {
+      txtBillDate.resignFirstResponder()
     }
     
     @IBAction func segmentbilltype(_ sender: UISegmentedControl) {
         
         if sender.selectedSegmentIndex == 0
         {
-            
-            txtMobilePlan.isHidden = true
-            txtMobileGBUsed.isHidden = true
-            txtMobileGBUsed.isHidden = true
-            txtMobileNumber.isHidden = true
+            txtBillID.isHidden = false
+            txtBillDate.isHidden = false
             txtMobileManufacturer.isHidden = true
+            txtMobileMinuteUsed.isHidden = true
+            txtMobileNumber.isHidden = true
+            txtMobileGBUsed.isHidden = true
+            txtMobilePlan.isHidden = true
+            txtInternetProviderName.isHidden = true
+            txtInternetGBUsed.isHidden = true
+            txtHydroAgency.isHidden = false
+            txtHydroUnitConsumed.isHidden = false
+        }
+        else if sender.selectedSegmentIndex == 1
+        
+        {
+             txtBillID.isHidden = false
+             txtBillDate.isHidden = false
+             txtMobileManufacturer.isHidden = true
+             txtMobileMinuteUsed.isHidden = true
+             txtMobileNumber.isHidden = true
+             txtMobileGBUsed.isHidden = true
+             txtMobilePlan.isHidden = true
+             txtInternetProviderName.isHidden = false
+             txtInternetGBUsed.isHidden = false
+             txtHydroAgency.isHidden = true
+             txtHydroUnitConsumed.isHidden = true
             
         }
+        else if sender.selectedSegmentIndex == 2
+        {
+             txtBillID.isHidden = false
+             txtBillDate.isHidden = false
+             txtMobileManufacturer.isHidden = false
+             txtMobileMinuteUsed.isHidden = false
+             txtMobileNumber.isHidden = false
+             txtMobileGBUsed.isHidden = false
+             txtMobilePlan.isHidden = false
+             txtInternetProviderName.isHidden = true
+             txtInternetGBUsed.isHidden = true
+             txtHydroAgency.isHidden = true
+             txtHydroUnitConsumed.isHidden = true
+        }
+        
+    }
+    
+    @IBAction func btnAddBill(_ sender: Any) {
+        //customer?.addBill(bill:, billID: <#T##String#>)
+        
+        
         
     }
     
